3ba55f7a82798c41c907fd9ceb27fbcf4c9daf69
Dieter <dieter.jordens@continuum.be>
2019-11-09 12:35:18 +0100

    uppercase start

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -196,7 +196,7 @@ insert into blogpost (title, publication_date, url, category)
 values ('blogpost.title.1', TO_DATE('12/02/2018', 'DD/MM/YYYY'), 'https://www.continuum.be/2018/02/12/hey-alexa-order-us-two-pizzas-please/', 'AI'),
        ('Machine learning on source code', TO_DATE('08/06/2018', 'DD/MM/YYYY'), 'https://www.continuum.be/2018/06/08/machine-learning-on-source-code/', 'AI'),
        ('Deep Instinct – Malware detection using deep learning', TO_DATE('02/11/2018', 'DD/MM/YYYY'), 'https://www.continuum.be/2018/11/02/deep-instinct-malware-detection-using-deep-learning/', 'AI'),
-       ('Destructuring', TO_DATE('08/05/2019', 'DD/MM/YYYY'), 'https://www.continuum.be/2019/05/08/destructuring-javascript/', 'frontend')
+       ('Destructuring', TO_DATE('08/05/2019', 'DD/MM/YYYY'), 'https://www.continuum.be/2019/05/08/destructuring-javascript/', 'Frontend')
 ;
 insert into translated_value (language, key, value)
 values ('en', 'blogpost.title.1', 'Hey Alexa! Order us two pizzas please.'),
