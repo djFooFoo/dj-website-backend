8664e87285a83fcfbe5f75a6d7bebca1c759c4a8
Dieter <dieter.jordens@continuum.be>
2019-11-20 13:02:39 +0100

    add certificate data to website.

--- src/main/resources/certificate-data.sql
+++ src/main/resources/certificate-data.sql
@@ -1,7 +1,7 @@
 insert into certificate (name, issue_date, organization, credential_url, certificate_type)
 values ('Oracle Certified Associate, Java SE 8 Programmer', TO_DATE('09/2016', 'MM/YYYY'), 'Oracle', 'https://www.youracclaim.com/badges/3ec8731f-0c1a-4075-bdf0-b6c8d5f0cbe3/linked_in_profile', 1),
        ('Professional Scrum Developer I', TO_DATE('04/2017', 'MM/YYYY'), 'Scrum.org', 'https://www.scrum.org/user/243806', 1),
-       ('EXIN Agile Scrum Foundation', TO_DATE('09/2019', 'MM/YYYY'), 'Exin Agile Scrum', 'https://exeed.pro/badge/43740', 1),
+       ('EXIN Agile Scrum Foundation', TO_DATE('09/2019', 'MM/YYYY'), 'Exin', 'https://exeed.pro/badge/43740', 1),
        ('deep.learning.specialization', TO_DATE('05/2019', 'MM/YYYY'), 'Coursera', 'https://www.coursera.org/account/accomplishments/specialization/SXJNRZSW2FNQ', 0),
        ('ai.for.everyone', TO_DATE('03/2019', 'MM/YYYY'), 'Coursera', 'https://www.coursera.org/account/accomplishments/verify/FL5WAVUSXLXE', 0),
        ('angular.challenge',  TO_DATE('05/2019', 'MM/YYYY'), 'NG-BE, Google, Innovatus, Showpad, EDITx', 'https://photos.google.com/share/AF1QipMGr_Fnj3VeEW0UJ7GJ_71Imb5zBI6m8JhO99nQPDmf_5mOqX9EtOi4rlBuEtU18Q?key=QXplbTItTExCZGhrd0FCNGMzc3Jwd05EVTJRYVlB', 2)
