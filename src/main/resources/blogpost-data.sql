f368ab4f4d10b3af76d848354b1849a3f9d07161
Dieter <dieter.jordens@continuum.be>
2020-04-01 11:21:11 +0200

    add blogpost to blogpost-data.sql

--- src/main/resources/blogpost-data.sql
+++ src/main/resources/blogpost-data.sql
@@ -11,7 +11,8 @@ values ('blogpost.title.1', TO_DATE('12/02/2018', 'DD/MM/YYYY'), 'https://www.co
        ('blogpost.title.10', TO_DATE('08/02/2020', 'DD/MM/YYYY'), 'https://medium.com/better-programming/how-to-create-an-angular-dockerfile-75c059e7f8e8?source=friends_link&sk=363c8b21aafe83e1e5f84feed047e969', 'Frontend'),
        ('blogpost.title.11', TO_DATE('21/02/2020', 'DD/MM/YYYY'), 'https://medium.com/better-programming/what-is-modularity-in-java-b1938a2304fd?source=friends_link&sk=202d13eb91c66a8fbec5580290b89c45', 'Backend'),
        ('blogpost.title.12', TO_DATE('10/03/2020', 'DD/MM/YYYY'), 'https://medium.com/better-programming/how-does-docker-port-binding-work-b089f23ca4c8?source=friends_link&sk=40c9fbcdc4172bb065711ab554e9b1c2', 'Programming'),
-       ('blogpost.title.13', TO_DATE('22/03/2020', 'DD/MM/YYYY'), 'https://towardsdatascience.com/increase-your-face-recognition-models-accuracy-by-improving-face-contrast-a3e71bb6b9fb?source=friends_link&sk=a51fd37b38a9f9770fc424208c93a6a2', 'AI')
+       ('blogpost.title.13', TO_DATE('22/03/2020', 'DD/MM/YYYY'), 'https://towardsdatascience.com/increase-your-face-recognition-models-accuracy-by-improving-face-contrast-a3e71bb6b9fb?source=friends_link&sk=a51fd37b38a9f9770fc424208c93a6a2', 'AI'),
+       ('blogpost.title.14', TO_DATE('31/03/2020', 'DD/MM/YYYY'), 'https://medium.com/better-programming/test-driven-development-attempt-with-jest-and-angular-7e064b76ca2d?source=friends_link&sk=02e853220ce751e054222a67377ac804', 'Frontend')
 ;
 insert into translated_value (language, key, value)
 values ('en', 'blogpost.title.1', 'Hey Alexa! Order us two pizzas please.'),
@@ -63,3 +64,7 @@ values ('en', 'blogpost.title.13', 'Increase Your Face Recognition Model’s Acc
        ('nl', 'blogpost.title.13', 'Verhoog de nauwkeurigheid van uw gezichtsherkenningsmodel door het gezichtscontrast te verbeteren. Beelden voorbewerken voor betere resultaten'),
        ('fr', 'blogpost.title.13', 'Augmentez la précision de votre modèle de reconnaissance faciale en améliorant le contraste des visages. Prétraitement des images pour de meilleurs résultats');
 
+insert into translated_value (language, key, value)
+values ('en', 'blogpost.title.14', 'Test-Driven Development Attempt With Jest and Angular'),
+       ('nl', 'blogpost.title.14', 'Test-Driven Development Poging Met Jest en Angular'),
+       ('fr', 'blogpost.title.14', 'Tentative de développement piloté par les tests avec Jest et Angular');
