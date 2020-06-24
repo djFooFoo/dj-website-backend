d416a8348b4952377c7fcc204717c6719cefd5d1
Dieter <dieter.jordens@continuum.be>
2020-06-24 18:35:26 +0200

    add certificate to certificate-data.sql.

--- src/main/resources/certificate-data.sql
+++ src/main/resources/certificate-data.sql
@@ -5,10 +5,21 @@ values ('Oracle Certified Professional: Java SE 11 Programmer', TO_DATE('12/2019
        ('EXIN Agile Scrum Foundation', TO_DATE('09/2019', 'MM/YYYY'), 'Exin', 'https://exeed.pro/badge/43740', 1),
        ('deep.learning.specialization', TO_DATE('05/2019', 'MM/YYYY'), 'Coursera', 'https://www.coursera.org/account/accomplishments/specialization/SXJNRZSW2FNQ', 0),
        ('ai.for.everyone', TO_DATE('03/2019', 'MM/YYYY'), 'Coursera', 'https://www.coursera.org/account/accomplishments/verify/FL5WAVUSXLXE', 0),
-       ('angular.challenge',  TO_DATE('05/2019', 'MM/YYYY'), 'NG-BE, Google, Innovatus, Showpad, EDITx', 'https://photos.google.com/share/AF1QipMGr_Fnj3VeEW0UJ7GJ_71Imb5zBI6m8JhO99nQPDmf_5mOqX9EtOi4rlBuEtU18Q?key=QXplbTItTExCZGhrd0FCNGMzc3Jwd05EVTJRYVlB', 2)
+       ('angular.challenge',  TO_DATE('05/2019', 'MM/YYYY'), 'NG-BE, Google, Innovatus, Showpad, EDITx', 'https://photos.google.com/share/AF1QipMGr_Fnj3VeEW0UJ7GJ_71Imb5zBI6m8JhO99nQPDmf_5mOqX9EtOi4rlBuEtU18Q?key=QXplbTItTExCZGhrd0FCNGMzc3Jwd05EVTJRYVlB', 2),
+       ('pytorch.deep.learning', TO_DATE('05/2020', 'MM/YYYY'), 'Udemy', 'https://www.udemy.com/certificate/UC-ae1c4739-0ce8-4153-bc4d-9492d58aed8c/', 0)
 ;
 
 insert into translated_value (language, key, value)
-values ('fr', 'deep.learning.specialization', 'Spécialisation de l''apprentissage profond'), ('nl', 'deep.learning.specialization', 'Deep Learning Specialisatie'), ('en', 'deep.learning.specialization', 'Deep Learning Specialization'),
-       ('fr', 'ai.for.everyone', 'L''IA pour tous'), ('nl', 'ai.for.everyone', 'AI voor iedereen'), ('en', 'ai.for.everyone', 'AI For Everyone'),
-       ('fr', 'angular.challenge', 'Finaliste d''Angular Challenge'), ('nl', 'angular.challenge', 'Angular Challenge Finalist'), ('en', 'angular.challenge', 'Angular Challenge Finalist');
+values ('fr', 'deep.learning.specialization', 'Spécialisation de l''apprentissage profond'),
+       ('nl', 'deep.learning.specialization', 'Deep Learning Specialisatie'),
+       ('en', 'deep.learning.specialization', 'Deep Learning Specialization'),
+       ('fr', 'ai.for.everyone', 'L''IA pour tous'),
+       ('nl', 'ai.for.everyone', 'AI voor iedereen'),
+       ('en', 'ai.for.everyone', 'AI For Everyone'),
+       ('fr', 'angular.challenge', 'Finaliste d''Angular Challenge'),
+       ('nl', 'angular.challenge', 'Angular Challenge Finalist'),
+       ('en', 'angular.challenge', 'Angular Challenge Finalist'),
+       ('fr', 'pytorch.deep.learning', 'PyTorch: Deep Learning et Intelligence Artificielle'),
+       ('nl', 'pytorch.deep.learning', 'PyTorch: Deep Learning and Artificial Intelligence'),
+       ('en', 'pytorch.deep.learning', 'PyTorch: Deep Learning en Artificiële Intelligentie');
+
