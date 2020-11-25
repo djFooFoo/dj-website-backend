f1899c56a333dbea14e762b199e7dd3d31964647
dieter.jordens <dieter.jordens@continuum.be>
2020-11-25 12:15:49 +0100

    update article classifications

--- src/main/java/dj/personal/website/article/ArticleRunner.java
+++ src/main/java/dj/personal/website/article/ArticleRunner.java
@@ -63,28 +63,28 @@ public class ArticleRunner implements CommandLineRunner {
 				.title("Destructuring")
 				.publicationDate(LocalDate.of(2019, Month.MAY, 8))
 				.url("https://www.continuum.be/2019/05/08/destructuring-javascript/")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.5")
 				.publicationDate(LocalDate.of(2019, Month.DECEMBER, 7))
 				.url("https://medium.com/better-programming/fantastic-translations-and-where-to-find-them-421ee46f59db?source=friends_link&sk=7ddb4e05714d19bcf4671fbc0856fdd1")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.6")
 				.publicationDate(LocalDate.of(2020, Month.JANUARY, 9))
 				.url("https://medium.com/better-programming/how-to-create-a-websocket-in-python-b68d65dbd549?source=friends_link&sk=556dcc527edf1d277053396332a270bd")
-				.category("Software Development")
+				.category("Python")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.7")
 				.publicationDate(LocalDate.of(2020, Month.JANUARY, 22))
 				.url("https://medium.com/better-programming/what-goes-into-a-dockerfile-ff0ace591060?source=friends_link&sk=6a4bdaf97cb6e7e76afe8fc733a1023f")
-				.category("Software Development")
+				.category("Docker")
 				.build());
 
 		articles.add(Article.builder()
@@ -105,21 +105,21 @@ public class ArticleRunner implements CommandLineRunner {
 				.title("article.title.10")
 				.publicationDate(LocalDate.of(2020, Month.FEBRUARY, 8))
 				.url("https://medium.com/better-programming/how-to-create-an-angular-dockerfile-75c059e7f8e8?source=friends_link&sk=363c8b21aafe83e1e5f84feed047e969")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.11")
 				.publicationDate(LocalDate.of(2020, Month.FEBRUARY, 21))
 				.url("https://medium.com/better-programming/what-is-modularity-in-java-b1938a2304fd?source=friends_link&sk=202d13eb91c66a8fbec5580290b89c45")
-				.category("Software Development")
+				.category("Java")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.12")
 				.publicationDate(LocalDate.of(2020, Month.MARCH, 10))
 				.url("https://medium.com/better-programming/how-does-docker-port-binding-work-b089f23ca4c8?source=friends_link&sk=40c9fbcdc4172bb065711ab554e9b1c2")
-				.category("Software Development")
+				.category("Docker")
 				.build());
 
 		articles.add(Article.builder()
@@ -133,7 +133,7 @@ public class ArticleRunner implements CommandLineRunner {
 				.title("article.title.14")
 				.publicationDate(LocalDate.of(2020, Month.MARCH, 31))
 				.url("https://medium.com/better-programming/test-driven-development-attempt-with-jest-and-angular-7e064b76ca2d?source=friends_link&sk=02e853220ce751e054222a67377ac804")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
@@ -147,14 +147,14 @@ public class ArticleRunner implements CommandLineRunner {
 				.title("article.title.16")
 				.publicationDate(LocalDate.of(2020, Month.APRIL, 23))
 				.url("https://medium.com/better-programming/decouple-tests-with-data-attributes-c920606c5f27?source=friends_link&sk=26885722b8f4f1896a157e2fb5fd060d")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.17")
 				.publicationDate(LocalDate.of(2020, Month.APRIL, 29))
 				.url("https://medium.com/better-programming/how-to-create-a-generator-in-python-5ee587621879?source=friends_link&sk=0017a0f77f44b7f2e896673cfb31d18d")
-				.category("Software Development")
+				.category("Python")
 				.build());
 
 		articles.add(Article.builder()
@@ -175,21 +175,21 @@ public class ArticleRunner implements CommandLineRunner {
 				.title("article.title.20")
 				.publicationDate(LocalDate.of(2020, Month.JUNE, 10))
 				.url("https://medium.com/better-programming/how-to-create-an-angular-pipeline-with-jenkins-8040f1a0c0ee?source=friends_link&sk=1b5c4a7723fb46f94ded85eda2e8e2e0")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.21")
 				.publicationDate(LocalDate.of(2020, Month.JUNE, 15))
 				.url("https://medium.com/better-programming/how-to-create-a-star-rating-component-in-angular-ff32234ea531?source=friends_link&sk=4b636bbb2898a171844386bbac08a65e")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.22")
 				.publicationDate(LocalDate.of(2020, Month.JUNE, 23))
 				.url("https://medium.com/swlh/enable-angular-google-analytics-14acac59e998?source=friends_link&sk=c4d260fbdc705b9947c665b17a8e923e")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
@@ -203,28 +203,28 @@ public class ArticleRunner implements CommandLineRunner {
 				.title("article.title.24")
 				.publicationDate(LocalDate.of(2020, Month.AUGUST, 17))
 				.url("https://medium.com/better-programming/3-reasons-to-switch-to-fastapi-f9c788d017e5?source=friends_link&sk=746e465da51bc0af4a94fc337689a2ff")
-				.category("Software Development")
+				.category("Python")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.25")
 				.publicationDate(LocalDate.of(2020, Month.SEPTEMBER, 8))
 				.url("https://medium.com/better-programming/how-to-create-a-dockerfile-for-a-spring-boot-application-46878997382f?source=friends_link&sk=d7f93632a36b031d97998d15f0225b75")
-				.category("Software Development")
+				.category("Java")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.26")
 				.publicationDate(LocalDate.of(2020, Month.SEPTEMBER, 24))
 				.url("https://medium.com/better-programming/how-to-create-a-dockerfile-for-a-python-application-8d078b16bc9a?source=friends_link&sk=d365c0380159d068e84dd562fb60d82c")
-				.category("Software Development")
+				.category("Python")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.27")
 				.publicationDate(LocalDate.of(2020, Month.OCTOBER, 19))
 				.url("https://medium.com/better-programming/what-is-a-behaviorsubject-in-rxjs-f580e934a5b2?source=friends_link&sk=4251c5d5ad45a881a6bf131fddf82846")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		articles.add(Article.builder()
@@ -245,14 +245,14 @@ public class ArticleRunner implements CommandLineRunner {
 				.title("article.title.30")
 				.publicationDate(LocalDate.of(2020, Month.NOVEMBER, 11))
 				.url("https://medium.com/better-programming/how-to-create-a-csv-file-in-python-aff1773b2de1?source=friends_link&sk=31c7ddaf9001264f6339540bfda91ec8")
-				.category("Software Development")
+				.category("Python")
 				.build());
 
 		articles.add(Article.builder()
 				.title("article.title.31")
 				.publicationDate(LocalDate.of(2020, Month.NOVEMBER, 12))
 				.url("https://medium.com/better-programming/how-to-create-a-hamburger-menu-in-angular-370b8ebdd7bb?source=friends_link&sk=756eac646e6cc5a3981c5ddf4934aeaa")
-				.category("Software Development")
+				.category("Angular")
 				.build());
 
 		return articleRepository.saveAll(articles);
