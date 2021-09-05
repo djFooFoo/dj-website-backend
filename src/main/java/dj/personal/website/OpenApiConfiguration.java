c52533dea1d239b30d158092800f134994c92478
Dieter Jordens <dieter.jordens.website@gmail.com>
2021-09-05 08:53:03 +0200

    add basic auth option to swagger

---
+++ src/main/java/dj/personal/website/OpenApiConfiguration.java
@@ -0,0 +1,21 @@
+package dj.personal.website;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "basicAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "basic"
+)
+@OpenAPIDefinition(
+        info = @Info(title = "Dieter Jordens API", version = "1.0.0"),
+        security = @SecurityRequirement(name = "basicAuth")
+)
+public class OpenApiConfiguration {
+}
